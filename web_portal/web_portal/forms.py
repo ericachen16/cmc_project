3f5f3babca27719f31483d43a383abefc41786dc
Erica Chen <ericachen@MBP.local>
2024-07-18 17:10:54 -0400

    both plots premulticlass

--- web_portal/web_portal/forms.py
+++ web_portal/web_portal/forms.py
@@ -4,8 +4,6 @@ from .models import SampleSource
 
 class SelectionForm(forms.Form):
     names = SampleSource.objects.all()
-    print('helo')
-    print(names)
     options = []
     for n in names:
         options.append((n.name,n.name))
--- web_portal/web_portal/views.py
+++ web_portal/web_portal/views.py
@@ -5,6 +5,7 @@ from .models import UnknownMolecule, Molecule
 from .forms import SelectionForm
 
 import pandas as pd
+pd.options.plotting.backend = "plotly"
 
 # import plotly.express as px
 
@@ -14,9 +15,13 @@ def getSelectedSamples(request):
         form = SelectionForm(request.POST)
         if form.is_valid():
             requestedSample = form.cleaned_data["selected"]
-            res = getUnknownMoleculeData(requestedSample)
-            plot = generatePlot(res)
-            return render(request, 'boxplots/sampleForm.html', {'form': form, 'plot':plot})
+            unkRes = getUnknownMoleculeData(requestedSample)
+            globRes = getMoleculeData(requestedSample)
+
+            plot1 = plotUnknown(unkRes)
+            # plot2 = plotUnknown(unkRes)
+            plot2 = plotGlobal(globRes)
+            return render(request, 'boxplots/sampleForm.html', {'form': form, 'plot1':plot1, 'plot2':plot2})
     else:
         form = SelectionForm()
         print("get")
@@ -25,40 +30,69 @@ def getSelectedSamples(request):
     return render(request, 'boxplots/sampleForm.html', {'form': form})
 
 def getUnknownMoleculeData(sampleNames):
-    result = []
+    # result = []
     df_list = []
     
     for n in sampleNames:
         i = UnknownMolecule.objects.all().filter(sampleSource__icontains=n).values()
-        result.append(i)
+        # result.append(i)
         df = pd.DataFrame(i.values('levDistance','type','numOfReads'))
         df['levDistance'] = df['levDistance'].replace({'Levenshtein': 'LV'}, regex=True)
         df["lev_type"] = df["levDistance"] + " | " + df["type"]
  
         df_list.append(df)
     
-    unMerged = df_list[0]
-    for df_ in df_list[1:]:
-        unMerged = unMerged.merge(df_, on="lev_type", how='outer')
+    # unMerged = df_list[0]
+    # for df_ in df_list[1:]:
+    #     unMerged = unMerged.merge(df_, on="lev_type", how='outer')
     #     dfMain = pd.concat(dfMain, df_, join='outer')
    
     dfMain = pd.concat(df_list, ignore_index=True)
-    dfMain = dfMain.groupby('lev_type', as_index=False)['numOfReads'].sum()
+    # dfMain = dfMain.groupby('lev_type', as_index=False)['numOfReads'].sum()
+    # dfMain['percentage'] = (dfMain['numOfReads']/dfMain['numOfReads'].sum())*100
     # dfFinal = dfMain.groupby('lev_type')['numOfReads'].sum()
     # print(df)
     # for i in result:
     #     print(pd.DataFrame(i.values('levDistance','type','numOfReads')))
-    print(unMerged)
+    dfMain.drop(df.columns[[0, 1]], axis=1, inplace=True)
+
+    dfMain['percent of reads (unnormalized)'] = (dfMain['numOfReads']/dfMain['numOfReads'].sum())*100
+    dfMain.rename(columns={'lev_type':'type'}, inplace=True)
     print(dfMain)
-    return
+    return dfMain
 
 
 def getMoleculeData(sampleNames):
-    result = []
+    # result = []
+    df_list = []
+    
     for n in sampleNames:
-        result.append(Molecule.objects.all().filter(sampleSource__icontains=n).values())
+        i = Molecule.objects.all().filter(sampleSource__icontains=n).values()
+        # result.append(i)
+        df = pd.DataFrame(i.values('description','numOfReads')) 
+        df_list.append(df)
+   
+    dfMain = pd.concat(df_list, ignore_index=True)
+    dfMain['percent of reads (unnormalized)'] = (dfMain['numOfReads']/dfMain['numOfReads'].sum())*100
+    dfMain.rename(columns={'description':'type'}, inplace=True)
+    print(dfMain)
+    return dfMain
 
-    return result
+def getFormattedDataFrame(df):
 
-def generatePlot(data):
-    return px.box(data)
+    return df
+
+
+# def getPlot(df, n):
+#     df.
+
+def plotUnknown(df):
+    # px.box(df, y=df.iloc[0])
+    return px.box(df, y=df['percent of reads (unnormalized)'], x=df['type'], width=1200, height=600,color = 'type',title="SummaryTable:UNKOnly").to_html()
+
+def plotGlobal(df):
+    return px.box(df, y=df['percent of reads (unnormalized)'], x=df['type'], width=1200, height=600,color = 'type', title="SummaryTable:GlobalView").to_html()
+
+# def renameCol(df):
+#     df.columns = ['percent of reads (unnormalized)', 'type','percent of reads (unnormalized)']
+#     return df
